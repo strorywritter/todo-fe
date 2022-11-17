a7ca8762c47bc5ef7757f32eae64e1d9082ed819
strorywritter <nramyashan@gmail.com>
2022-11-18 02:57:38 +0530

    date range npm modules & ui updated

---
+++ src/components/dateRangePicker.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+export default function BasicDateRangePicker({value,setValue}) {
+
+
+  return (
+
+    <div style={{ padding:'20px'}}>
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      localeText={{ start: 'Check-in', end: 'Check-out' }}
+    >
+      <DateRangePicker
+        value={value}
+        // formatDate={(date) => console.log(date.toISOString())}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(startProps, endProps) => (
+          <React.Fragment>
+            <TextField {...startProps} />
+            <Box sx={{ mx: 2 }}> to </Box>
+            <TextField {...endProps} />
+          </React.Fragment>
+        )}
+      />
+    </LocalizationProvider>
+    </div>
+
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,11 @@ import ToDoList from "../components/toDoList";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import BasicDateRangePicker from "../components/dateRangePicker";
+import { Button } from "@mui/material";
 
 function Home({ token, email }) {
   const [tasks, setTasks] = useState([]);
@@ -14,6 +19,14 @@ function Home({ token, email }) {
   const [triggerFetch,settriggerFetch] = useState(false)
   // const [email, setEmail] = useState("");
   const [file, setFile] = useState();
+  const [filterOption, setFilterOption] = React.useState('all');
+  const [value, setValue] = React.useState([null, null]);
+
+  const handleChange = (event) => {
+    setFilterOption(event.target.value);
+    settriggerFetch(!triggerFetch)
+  };
+
   const fetch = async (token) => {
     await axios
       .get(`http://localhost:4000/user/getTodo?email=${email}`, {
@@ -27,9 +40,70 @@ function Home({ token, email }) {
         setLoading(false);
       });
   };
+
+  const getTodoByStatus = async (token,status) => {
+    await axios
+      .get(`http://localhost:4000/user/getTodoByStatus?status=${status}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setTasks(res.data);
+        setLoading(false);
+      });
+  };
+
+  const getTodoByDate = async (token) => {
+    const bodyData = {
+      startDate: value[0].$d.toISOString(),
+      endDate: value[1].$d.toISOString()
+    }
+    // bodyData.append("startDate", value[0].$d.toISOString().split('Z')[0]);
+    // bodyData.append("endDate", value[1].$d.toISOString().split('Z')[0]);
+    await axios
+      .post(`http://localhost:4000/user/getTodoByDate`,bodyData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setTasks(res.data);
+        setLoading(false);
+      });
+  };
+
+
+
+  const updateTaskList = (token) => {
+    switch (filterOption){
+      case "all":{
+        fetch(token);
+        break;
+      }
+      case 'todo':{
+        getTodoByStatus(token,'Todo')
+        break;
+      }
+      case 'inProgress':{
+        getTodoByStatus(token,'In progress')
+        break;
+      }
+      case 'done':{
+        getTodoByStatus(token,'Done')
+        break;
+      }
+      default:{
+        fetch(token);
+      }
+    }
+  }
+
   useEffect(() => {
     if (token) {
-      fetch(token);
+      updateTaskList(token);
     }
   }, [token,triggerFetch]);
 
@@ -78,6 +152,25 @@ function Home({ token, email }) {
     <div>
       <CustomAppBar />
       <Grid container spacing={2} style={{margin:'10px'}}>
+        <Grid item xs={6}>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={filterOption}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={'all'}>All</MenuItem>
+          <MenuItem value={'todo'}>Todo</MenuItem>
+          <MenuItem value={'inProgress'}>In progress</MenuItem>
+          <MenuItem value={'done'}>Done</MenuItem>
+        </Select>
+
+        </Grid>
+        <Grid>
+        <BasicDateRangePicker value={value} setValue={setValue}/>
+        <Button onClick={()=>getTodoByDate(token)}>Filter</Button>
+        </Grid>
         <Grid item xs={6} >
           <ToDoList tasks={tasks} onChange={taskUpdate} />
         </Grid>
